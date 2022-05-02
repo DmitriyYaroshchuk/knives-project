60d343f7e5375a6309ce1d85ad8bbfa9cb55237a
Dmitriy Yaroshchuk <95653054+DmitriyYaroshchuk@users.noreply.github.com>
2022-05-02 10:31:23 +0300

    Add files via upload

---
+++ product/scripts/main.js
@@ -0,0 +1,28 @@
+$('.showcase__container').slick({
+    dots:false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode:true
+});
+let modal = document.querySelector('#modal');
+let modalButton = document.querySelector('#modalButton');
+let modalClose = document.querySelector('#modalClose');
+let modalCurtain = document.querySelector('#modalCurtain')
+
+modalButton.addEventListener('click',function () {
+    modal.classList.add('modal__active');
+})
+modalClose.addEventListener('click',function () {
+    modal.classList.remove('modal__active');
+
+})
+modalCurtain.addEventListener('click',function () {
+    modal.classList.remove('modal__active');
+
+})
+
+
+
+
